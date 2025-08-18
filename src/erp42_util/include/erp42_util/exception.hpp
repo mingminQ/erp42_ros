c80def1fc9ec9840734d8dde4a01c35526d54449
mingminQ <mgkilprg@gmail.com>
2025-08-19 08:56:19 +0900

    Update exception.hpp

--- src/erp42_util/include/erp42_util/exception.hpp
+++ src/erp42_util/include/erp42_util/exception.hpp
@@ -1,16 +1,27 @@
 /**
- * ------------------------------------------------------------
+ * -------------------------------------------------------------------------------------------------
+ * 
+ * Copyright 2025 Minkyu Kil
  *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
  * @file    exception.h
  * @brief   Exception definition
  * @author  Minkyu Kil
  * @date    2025-07-01
  * @version 1.0
  *
- * Copyright (c) 2025, Minkyu Kil
- * All rights reserved
- *
- * ------------------------------------------------------------
+ * -------------------------------------------------------------------------------------------------
  */
 
 #ifndef ERP42_UTILS_ECXEPTION_H_
