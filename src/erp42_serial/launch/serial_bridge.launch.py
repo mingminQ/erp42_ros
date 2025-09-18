891986d0ddfd0fd63a15d2c10f319f7eee830750
mingminQ <mgkilprg@gmail.com>
2025-09-19 06:15:22 +0900

    Add erp42_serial_bridge parameter file

--- src/erp42_serial/launch/serial_bridge.launch.py
+++ src/erp42_serial/launch/serial_bridge.launch.py
@@ -3,40 +3,30 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
-
 from launch_ros.actions import Node
+
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
     
-    # Serial port settings
-    port_path = DeclareLaunchArgument('port_path', default_value = '/dev/ttyUSB0')
-    baud_rate = DeclareLaunchArgument('baud_rate', default_value = '115200'      )
+    # ERP42 serial bridge parameter file
+    erp42_serial_bridge_parameter_file = DeclareLaunchArgument('erp42_serial_bridge_parameter_file', 
+        default_value=PathJoinSubstitution([
+            FindPackageShare('erp42_serial'), 'config', 'serial_bridge.param.yaml'
+        ])
+    )
 
-    # ERP42 parameters
-    max_speed_mps       = DeclareLaunchArgument('max_speed_mps',      default_value  = '7.00' )
-    max_steering_deg    = DeclareLaunchArgument('max_steering_deg',    default_value = '25.00')
-    steering_offset_deg = DeclareLaunchArgument('steering_offset_deg', default_value = '0.00' )
-
-    # Serial bridge node
+    # ERP42 serial bridge
     erp42_serial_bridge = Node(
         package    = 'erp42_serial', 
         executable = 'serial_bridge', 
         name       = 'erp42_serial_bridge', 
         output     = 'screen',
-        parameters = [{
-            'port_path'          : LaunchConfiguration('port_path'          ),
-            'baud_rate'          : LaunchConfiguration('baud_rate'          ),
-            'max_speed_mps'      : LaunchConfiguration('max_speed_mps'      ),
-            'max_steering_deg'   : LaunchConfiguration('max_steering_deg'   ),
-            'steering_offset_deg': LaunchConfiguration('steering_offset_deg')
-        }]
+        parameters = [{LaunchConfiguration('erp42_serial_bridge_parameter_file')}]
     )
 
     return LaunchDescription([
-        port_path,
-        baud_rate,
-        max_speed_mps,
-        max_steering_deg,
-        steering_offset_deg,
+        erp42_serial_bridge_parameter_file,
         erp42_serial_bridge
     ])
